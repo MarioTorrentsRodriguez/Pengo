79e0e4f72f78846ac1ae75d262bbe7753d81b4e3
victorpeor <victorperezortega21@gmail.com>
2025-04-07 09:49:57 +0200

    Asset player ( No arreglado tamaño )

--- Vikings/src/Player.cpp
+++ Vikings/src/Player.cpp
@@ -23,7 +23,7 @@ AppStatus Player::Initialise()
 	const int n = PLAYER_FRAME_SIZE;
 
 	ResourceManager& data = ResourceManager::Instance();
-	if (data.LoadTexture(Resource::IMG_PLAYER, "images/eric.png") != AppStatus::OK)
+	if (data.LoadTexture(Resource::IMG_PLAYER, "images/CharAnimations.png") != AppStatus::OK)
 		return AppStatus::ERROR;
 
 	render = new Sprite(data.GetTexture(Resource::IMG_PLAYER));
